cbd2155985ef834126a4d52256a4707c01ea080f
Alex Tran <Lockey5891@aol.com>
2024-05-14 19:20:19 -0700

    "merge all commit"

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ const fetchYoutubeApi = async function() {
 
 // Transfer Youtube Data to Local Storage
 // Redirect to index.html page
-button.addEventListener( "click", async function( event ) {
+searchBtn.addEventListener( "click", async function( event ) {
   event.preventDefault();
   const youtubeData = await fetchYoutubeApi();
   localStorage.setItem( "youtubeData", JSON.stringify( youtubeData ) );
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
       closeModal($modal);
     });
   }
-console.log(document)
+
   // Add a click event on buttons to open a specific modal
   (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
     const modal = $trigger.dataset.target;
